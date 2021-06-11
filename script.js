1211393911b3239ba23a8c10560f72584d14236b
Nazala <nazala5151@gmail.com>
2021-06-11 16:20:03 -0500

    html & css inital save of hw

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// var possibleChoices = ["Rock", "Paper", "Scissors"];
+
+// alert("Let's Play a Game!")
+// confirm("Yes or No?")
+
+// var response = prompt("Choose rock, paper, or scissors.").toLowerCase();
+
+// if (response == "Rock") {
+//     console.log("Rock!")
+// } else if (response == "Paper") {
+//     console.log("Paper!")
+// } else if (response === "scissors") {
+//     console.log("Scissors, dude")
+// }
+
+// var possibleChoices = ["Rock", "Paper", "Scissors"];
+
+// let randomIndex = Math.floor(Math.random()*possibleChoices.length)
+// var computerChoice = possibleChoices[randomIndex];
+
+// console.log(Math.floor(Math.random()*possibleChoices.length));
+    
+// alert(computerChoice);
+
+// if (response === computerChoice) {
+//     alert("Tied!")
