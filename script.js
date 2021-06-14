97a6ffe3ee72110cc5d0f1782ecde9b40c117667
Nazala <nazala5151@gmail.com>
2021-06-14 10:52:54 -0500

    first JS save

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 
+const button=document.querySelector("#button")
+button.addEventListener("click", GeneratePassword)
+console.log(button)
+
+function GeneratePassword() {
+    var possibleChoices = []
+    var possibleUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]
+    var response = prompt("Choose the character length of your password. Choose between 8 - 128 below.")
+    var Uppercase = confirm("Do you want Uppercase characters included in your password? Click OK to choose and cancel to exclude but continue.")
+    console.log(Uppercase)
+    var Lowercase = confirm("Do you want Lowercase characters included in your password? Click OK to choose and cancel to exclude but continue.")
+    console.log(Lowercase)
+    var Numeric = confirm("Do you want Numeric characters included in your password? Click OK to choose and cancel to exclude but continue.")
+    console.log(Numeric)
+    var SpecialCharacters = confirm("Do you want Special Characters characters included in your password? Click OK to choose and cancel to exclude but continue.")
+    console.log(SpecialCharacters)
+}
+
+if (Uppercase === true) {
+    
+   }
+
+if (Lowercase === true) {
+}
+
+
+
+
+
+
+
 // var possibleChoices = ["Rock", "Paper", "Scissors"];
 
 // alert("Let's Play a Game!")
